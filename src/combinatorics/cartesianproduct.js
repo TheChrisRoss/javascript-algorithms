230479b80cabc65a4299186b19fb518f9c9b3a1d
Chris Ross <thechrisross@gmail.com>
2021-04-13 22:29:11 -0500

    updated quotes.  fixed const/lets

--- src/combinatorics/cartesianproduct.js
+++ src/combinatorics/cartesianproduct.js
@@ -1,15 +1,15 @@
 (function (exports) {
-  'use strict';
+  "use strict";
   exports.cartesianProduct = (function () {
     let result;
 
-    function cartesianProduct (sets, index, current) {
+    function cartesianProduct(sets, index, current) {
       if (index === sets.length) {
-        return result.push (current.slice ());
+        return result.push(current.slice());
       }
-      for (var i = 0; i < sets[index].length; i += 1) {
+      for (let i = 0; i < sets[index].length; i += 1) {
         current[index] = sets[index][i];
-        cartesianProduct (sets, index + 1, current);
+        cartesianProduct(sets, index + 1, current);
       }
     }
 
@@ -38,9 +38,8 @@
      */
     return function (sets) {
       result = [];
-      cartesianProduct (sets, 0, []);
+      cartesianProduct(sets, 0, []);
       return result;
     };
-  } ());
-
-}((typeof window === 'undefined') ? module.exports : window));
+  })();
+})(typeof window === "undefined" ? module.exports : window);
